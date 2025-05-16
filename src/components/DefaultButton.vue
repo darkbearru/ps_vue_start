fff3068fba96a9ae9f2c467d88175e9ab5a8b68f
Alexey Abramenko <a.abramenko@chita.ru>
2025-05-16 14:01:34 +0300

    Add Button Component

---
+++ src/components/DefaultButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+const { color = 'var(--color-primary)' } = defineProps({ color: String });
+</script>
+
+<template>
+	<button class="default-button" :style="{ backgroundColor: color }">
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+.default-button {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-width: 300px;
+	height: 68px;
+	border: none;
+	border-radius: 100px;
+	color: var(--color-primary-inverted);
+	font-size: 24px;
+	cursor: pointer;
+}
+</style>
